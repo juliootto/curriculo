40a727ac536e800b9659e86003a194e7a561401b
juliootto <52258494+juliootto@users.noreply.github.com>
2024-07-03 21:15:55 -0300

    a

--- php/config.php
+++ php/config.php
@@ -3,6 +3,6 @@
 //Constantes para armazenamento das variáveis de conexão.
 define('HOST', 'localhost');
 define('PORT', '3306');
-define('DBNAME', 'julott_teste');
-define('USER', 'julott_teste');
-define('PASSWORD', '002232');
+define('DBNAME', 'DB');
+define('USER', 'USER');
+define('PASSWORD', 'PASS');
