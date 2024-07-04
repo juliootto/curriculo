75b80ebd66a2de26bda8b7ec19ae4555546df0ba
juliootto <52258494+juliootto@users.noreply.github.com>
2024-07-03 21:16:19 -0300

    Delete php/config.php

--- php/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-/***CONEXÃO COM O BD ***/
-//Constantes para armazenamento das variáveis de conexão.
-define('HOST', 'localhost');
-define('PORT', '3306');
-define('DBNAME', 'julott_teste');
-define('USER', 'julott_teste');
-define('PASSWORD', '002232');
