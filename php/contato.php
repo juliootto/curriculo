248733afcf01247dd233408e4094229530d7f7eb
juliootto <52258494+juliootto@users.noreply.github.com>
2024-07-04 12:52:29 -0300

    incluida a funcionalidade de envio de email

--- php/contato.php
+++ php/contato.php
@@ -1,5 +1,14 @@
 <?php
 include "config.php";
+require './PHPMailer/src/Exception.php';
+require './PHPMailer/src/PHPMailer.php';
+require './PHPMailer/src/SMTP.php';
+
+use PHPMailer\PHPMailer\Exception;
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+
+
 /***VALIDAÇÃO DOS DADOS RECEBIDOS DO FORMULÁRIO ***/
 
 if($_REQUEST['nome'] == ""){
@@ -37,11 +46,41 @@ $stmt = $dsn->prepare("INSERT INTO
 
 $resultSet = $stmt->execute([$_REQUEST['nome'], $_REQUEST['email'], $_REQUEST['phone'], $_REQUEST['message']]);
 
-/*if($resultSet){
-	echo "<script>alert('Dados enviados com Sucesso!');";
+if($resultSet){
+	echo "Dados salvos no banco com sucesso";
 }else{
-	echo "<script>alert('Erro ao enviar os Dados!');</script>";
-}*/
+	echo "Erro ao enviar os Dadosno banco!";
+}
+
+$mail = new PHPMailer(true);
+$mensagem_email = "<p>Dados do contato:</p> <p>Nome: ".$_REQUEST['nome']."</p><p>Email: ".$_REQUEST['email']."</p><p>Telefone: ".$_REQUEST['phone']."</p><p>Mensagem:</p><p>".$_REQUEST['message']."</p>";
+
+try {
+
+	$mail->isSMTP();
+	$mail->Host       = HOST_EMAIL;
+	$mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+	$mail->Port       = PORT_EMAIL;
+
+	$mail->SMTPAuth   = true;
+	$mail->Username   = USER_EMAIL;
+	$mail->Password   = PASS_EMAIL;
+
+	$mail->setFrom($_REQUEST['email'], $_REQUEST['nome']);
+	$mail->addAddress(USER_EMAIL);
+
+	$mail->isHTML(true);
+	$mail->Subject = 'Contato pelo site curriculo';
+	$mail->Body    = $mensagem_email;
+
+	$mail->send();
+	echo 'Email enviado com sucesso';
+} catch (Exception $e) {
+
+	echo "A mensagem não pôde ser enviada. Erro do PHPMailer: {$mail->ErrorInfo}";
+
+}
+
 
 //Destruindo o objecto statement e fechando a conexão
 $stmt = null;
